12567cdf62e1b0dee96ed234631b784258f17243
fzhao888 <frank.zhao93@gmail.com>
2023-09-11 23:28:37 -0500

    Added api/thoughts routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,18 @@
 const router = require('express'). Router();
 
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+} = '../../controllers/thoughtController.js';
 
+// get all thoughts
+router.route('/').get(getThoughts);
+
+// get single thought
+router.route('/:thoughtId').get(getSingleThought);
+
+// create thought
+router.route('/:thoughtId').get(getSingleThought).post(createThought);
 
 module.exports = router;
